bf8ebc14b13eec933debd5340e044193595673eb
cosmic-slice <148008605+cosmic-slice@users.noreply.github.com>
2025-10-14 02:52:46 +0000

    Fixed mli file for hw2

--- logic/hw2_tictactoe_logic.mli
+++ logic/hw2_tictactoe_logic.mli
@@ -1,73 +1,60 @@
 open! Core
 
-module Player_kind : sig
+module Players : sig
   type t =
-    | X
-    | O
+    | PlayerOne
+    | PlayerTwo
   [@@deriving sexp, compare, equal]
 
   val opposite : t -> t
 end
 
-module Cell_position : sig
-  type t =
-    { row : int
-    ; column : int
-    }
-  [@@deriving sexp, compare]
-
-  (* Defines a [Cell_position.Map.t]. *)
-  include Comparable.S with type t := t
-end
-
-module Move : module type of Cell_position
-
 module Decision : sig
   type t =
-    | In_progress of { whose_turn : Player_kind.t }
-    | Winner of Player_kind.t
-    | Stalemate
+    | Playing of { whose_turn : Players.t }
+    | Winner of Players.t
+    | Tie
   [@@deriving sexp, compare, equal]
 
   val is_game_over : t -> bool
 end
-
 module Game_state : sig
   type t =
-    { board : Player_kind.t Cell_position.Map.t
-    ; rows : int
-    ; columns : int
-    ; winning_sequence_length : int
+    { player_one_side: int array
+    ; player_two_side: int array
+    ; player_one_score: int
+    ; player_two_score: int
+    ; num_squares_per_side: int
+    ; init_beads: int
     ; decision : Decision.t
-    ; last_move : Move.t option (* For animation purposes. *)
+    ; last_move : int option (* For animation purposes. *)
     }
   [@@deriving sexp, compare, equal]
-
   module Create_error : sig
     type t =
       | Board_too_big_or_small
-      | Unwinnable_sequence_length
+    [@@deriving sexp, compare]
+  end
+
+  module Move_error : sig
+    type t = 
+      | Game_is_over
+      | Not_a_valid_square
+      | Square_is_empty
     [@@deriving sexp, compare]
   end
 
   val create
-    :  rows:int
-    -> columns:int
-    -> winning_sequence_length:int
+    :  num_squares_per_side:int
+    -> init_beads:int
     -> (t, Create_error.t list) Result.t
 
-  module Move_error : sig
-    type t =
-      | Game_is_over
-      | Space_already_filled
-      | Illegal_cell_position
-    [@@deriving sexp, compare]
-  end
-
-  val get_all_moves : t -> Move.t list
-  val make_move : t -> Move.t -> (t, Move_error.t) Result.t
-
-  module For_testing : sig
-    val all_directions : (int * int) list
-  end
+  val is_square_empty : t -> int -> bool
+  val is_game_over : t -> bool
+  val get_current_player : t -> Players.t option
+  val check_winner : t -> t option
+  val is_valid_square : t -> int -> bool
+  val distribute_beads : t -> int -> (t, Move_error.t) Result.t
+  val make_move : t -> int -> (t, Move_error.t) Result.t
+  
 end
